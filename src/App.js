eb2fd3627efec01c7bff6f8784d268260692c380
ThapaVinay <thechampthapa@gmail.com>
2023-06-09 13:39:24 +0530

    alerts added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 
 import React,{ useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
   const [mode, setMode] = useState('light');  // whether dark mode is enabled
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message,type) => {
+    setAlert({
+      message : message,
+      type : type
+    })
+    setTimeout( () => {
+      setAlert(null)
+    },1500);
+  }
 
   const toggleMode = () => {
     if(mode === 'light')
     {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled ", "success")
     }else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled ", "success")
     }
   }
 
   return (
     <>
       <Navbar title="TextUtility" mode = {mode} toggleMode = {toggleMode}/>
-
+      <Alert alert = {alert} />
       <div className="container my-3">
-        <TextForm heading = "Enter The Text To Analyse " mode = {mode}/>
+        <TextForm showAlert = {showAlert} heading = "Enter The Text To Analyse " mode = {mode}/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,37 +1,39 @@
 // hooks allows you to use class based components state feature in function based components
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export default function TextForm(props) {
 
-    const handleUpClick  = () => {
+    const handleUpClick = () => {
         // console.log("Uppercase was clicked " + text);
         let newText = text.toUpperCase();
         setText(newText)
+        
     }
 
-    const handleOnChange  = (event) => {
+    const handleOnChange = (event) => {
         // console.log("On change ");
         setText(event.target.value)
+        props.showAlert("Converted to Uppercase ", "success")
     }
 
-    const handleLoClick  = () => {
+    const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to Lowercase ", "success")
     }
 
-    const handleClearClick  = () => {
+    const handleClearClick = () => {
         setText("");
+        props.showAlert("Text Cleared ", "success")
     }
 
     const countWords = () => {
         let list = text.trim().split(" ");
         let count = 0;
-        for(let i=0; i<list.length; i++)
-        {
-            if(list[i] !== "")
-            {
-                count ++;
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] !== "") {
+                count++;
             }
         }
         return count;
@@ -39,25 +41,25 @@ export default function TextForm(props) {
 
     const [text, setText] = useState('');
     // setText("Enter text here");
-  return (
-    <>
-    <div className="container" style = {{color : props.mode === 'light' ? '#042743' : 'white'}}>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-        <textarea className="form-control" value = {text} onChange = {handleOnChange} style = {{color : props.mode === 'dark' ? 'white' : '#042743', backgroundColor : props.mode === 'dark' ? "grey" : "white"}} id="myBox" rows="8" placeholder='Enter your text'></textarea>
-        </div>
-        <button className="btn btn-primary mx-1 " onClick = {handleUpClick}> Convert to Uppercase</button>
-        <button className="btn btn-primary mx-1" onClick = {handleLoClick}> Convert to Lowercase</button>
-        <button className="btn btn-primary mx-1" onClick = {handleClearClick}> Clear Text</button>
-    </div>
-    <div className="container my-3" style = {{color : props.mode === 'light' ? '#042743' : 'white'}}>
-        <h2>Your text summary</h2>
-        {/* count the number of words and characters */}
-        <p> {countWords()} words and {text.length} characters </p>  
-        <p> {0.008 * countWords()} minutes to read</p>
-        <h2>Preview</h2>
-        <p>{text}</p>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="container" style={{ color: props.mode === 'light' ? '#042743' : 'white' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ color: props.mode === 'dark' ? 'white' : '#042743', backgroundColor: props.mode === 'dark' ? "grey" : "white" }} id="myBox" rows="8" placeholder='Enter your text'></textarea>
+                </div>
+                <button className="btn btn-primary mx-1 " onClick={handleUpClick}> Convert to Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLoClick}> Convert to Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleClearClick}> Clear Text</button>
+            </div>
+            <div className="container my-3" style={{ color: props.mode === 'light' ? '#042743' : 'white' }}>
+                <h2>Your text summary</h2>
+                {/* count the number of words and characters */}
+                <p> {countWords()} words and {text.length} characters </p>
+                <p> {0.008 * countWords()} minutes to read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
+    )
 }
